52a1cf90802061bd14bb553e0893d1ba5f77b228
Ryan Yao <theryanyao@gmail.com>
2023-03-11 16:36:52 -0500

    made it compile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,25 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Routes, Route }
 	from 'react-router-dom';
-import login from './login';
-import home from './home'
-
+import "./App.css";
+import Login from "./pages/Login";
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	Routes,
+	RouterProvider,
+} from "react-router-dom";
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path='/home' exact element={<home />} />
-				<Route path='/login' element={<login />} />
-			</Routes>
-		</Router>
-	);
-	import "./App.css";
-	import Login from "./pages/Login";
-	import {
-		createBrowserRouter,
-		createRoutesFromElements,
-		Route,
-		Routes,
-		RouterProvider,
-	} from "react-router-dom";
-	function App() {
-		return (
-			<div className="App">
-				<div className="background">
-					<Routes>
-						<Route path="/" element={<Login />} />
-					</Routes>
-				</div>
+		<div className="App">
+			<div className="background">
+				<Routes>
+					<Route path="/" element={<Login />} />
+				</Routes>
 			</div>
-		);
-	}
+		</div>
+	);
+}
 
-	export default App;
+export default App;
