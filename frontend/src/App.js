071144725e40a973a5aef746252ff513c6915423
Ryan Yao <theryanyao@gmail.com>
2023-03-11 16:33:36 -0500

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route }
 	from 'react-router-dom';
 import login from './login';
+import home from './home'
 
 function App() {
 	return (
 		<Router>
 			<Routes>
+				<Route path='/home' exact element={<home />} />
 				<Route path='/login' element={<login />} />
 			</Routes>
 		</Router>
